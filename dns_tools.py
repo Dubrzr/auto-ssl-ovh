d845180b92fa8f22d49c682d67e02ffd43a6c745
j.du <julien.dubiel@aphp.fr>
2017-12-01 15:06:52 +0100

    Add ACME https certificates generation

---
+++ dns_tools.py
@@ -0,0 +1,62 @@
+from structlog import get_logger
+from datetime import datetime, timedelta
+import dns.resolver
+import time
+import sys
+
+class BaseDns(object):
+    def __init__(self):
+        self.dns_provider_name = None
+        if self.dns_provider_name is None:
+            raise ValueError('The class attribute dns_provider_name ought to be defined.')
+
+    def create_dns_record(self, domain_name, base64_of_acme_keyauthorization):
+        raise NotImplementedError('create_dns_record method must be implemented.')
+
+    def delete_dns_record(self, domain_name, base64_of_acme_keyauthorization):
+        raise NotImplementedError('delete_dns_record method must be implemented.')
+
+
+class OVHDns(BaseDns):
+
+    def __init__(self, ovh_client):
+        self.dns_provider_name = 'OVH'
+        self.ovh_client = ovh_client
+
+    def create_dns_record(self, domain_name, base64_of_acme_keyauthorization):
+        s = domain_name.split('.')
+        zone = '.'.join(s[-2:])
+        subDomain = '_acme-challenge.' + '.'.join(s[:-2])
+        target = '"{}"'.format(base64_of_acme_keyauthorization)
+        try:
+            self.ovh_client.delete_record(zone, subDomain)
+        except:
+            pass
+        self.ovh_client.add_record(zone, subDomain, fieldType='TXT', target=target)
+        print("Added DNS record {}|{}|{}".format(subDomain, 'TXT', target))
+
+        print("NS lookups until {} DNS record is found".format(subDomain + '.' + zone), end='')
+        sys.stdout.flush()
+
+        timeout = 600
+        t = datetime.now()
+        while True:
+            try:
+                answers = dns.resolver.query(subDomain + '.' + zone, 'TXT')
+            except dns.resolver.NXDOMAIN:
+                answers = []
+            if len(answers) > 0 and answers[0] == target:
+                break
+            if timedelta.total_seconds(datetime.now() - t) > timeout:
+                raise Exception("DNS record is still unupdated after {} seconds. Exiting...".format(timeout))
+            time.sleep(5)
+            print('.', end='')
+            sys.stdout.flush()
+        print(' Found!')
+
+
+    def delete_dns_record(self, domain_name, base64_of_acme_keyauthorization):
+        s = domain_name.split('.')
+        zone = '.'.join(s[-2:])
+        subDomain = '.'.join(s[:-2])
+        self.ovh_client.delete_record(zone, '_acme-challenge.' + subDomain)
--- ssl-ovh.py
+++ ssl-ovh.py
@@ -1,20 +1,186 @@
 import os
 import json
 import ovh
+import ovh.exceptions   
+
+from ACMEclient import ACMEclient
+from dns_tools import OVHDns
+
+def create_folder(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+class OvhClient:
+    def __init__(self, endpoint, application_key, application_secret, consumer_key):
+        self._client = ovh.Client(
+            endpoint=conf['ovh_ids']['endpoint'],
+            application_key=conf['ovh_ids']['app_key'],
+            application_secret=conf['ovh_ids']['app_secret'],
+            consumer_key=conf['ovh_ids']['consumer_key']
+        )
+        self._cache = {}
+
+    @staticmethod
+    def give_rights(zone):
+        client = ovh.Client(
+            endpoint=conf['ovh_ids']['endpoint'],
+            application_key=conf['ovh_ids']['app_key'],
+            application_secret=conf['ovh_ids']['app_secret']
+        )
+        ck = client.new_consumer_key_request()
+        ck.add_recursive_rules(ovh.API_READ_WRITE, '/domain/zone/{}/*'.format(zone))
+        validation = ck.request()
+
+        print("Please visit {} to authenticate".format(validation['validationUrl']))
+        input("and press Enter to continue...")
+
+        print("Your 'consumerKey' is '{}'".format(validation['consumerKey']))
+
+        return validation['consumerKey']
+
+
+    def get_zone_mappings(self, zone, force_refresh=False):
+        if not force_refresh and zone in self._cache:
+            return self._cache[zone]
+
+        prfx =  '/domain/zone/{}'.format(zone)
+
+        entries_mappings = {}
+
+        for record_id in self._client.get(prfx + '/record'):
+            data = self._client.get(prfx + '/record/{}'.format(record_id))
+            entries_mappings[data['subDomain']] = data
+
+        self._cache[zone] = entries_mappings
+        return self._cache[zone]
+
+    def create_and_update_domains(self, domains):
+        for zone in domains:
+            print("Updating {} DNS entries...".format(zone['url']))
+            prfx =  '/domain/zone/{}'.format(zone['url'])
+
+            zone_mappings = self.get_zone_mappings(zone['url'])
+
+            for dns_entry in zone['dns_entries']:
+                subdomain = dns_entry['subDomain']
+                dn = subdomain + '.' + zone['url']
+
+                if subdomain not in zone_mappings:
+                    # We create the entry
+                    self.add_record(zone['url'], **dns_entry)
+
+                    self._client.post(prfx + '/record', **dns_entry)
+                    print("  -> Created DNS entry for {} ({})".format(dn, dns_entry))
+                else:
+                    data = zone_mappings[dns_entry['subDomain']]
+                    if self.update_record(zone['url'], **dns_entry):
+                        print("  -> Updated DNS entry for {} ({})".format(dn, dns_entry))
+
+            self.refresh_zone(zone['url'])
+            
+    def refresh_zone(self, zone):
+        self._client.post('/domain/zone/{}/refresh'.format(zone))
+        print("Refreshed zone {}".format(zone))
+
+    def add_record(self, zone, subDomain, fieldType, target, ttl=0):
+        zone_mappings = self.get_zone_mappings(zone)
+
+        if subDomain not in zone_mappings:
+               # We create the entry
+            self._client.post('/domain/zone/{}/record'.format(zone), 
+                   subDomain=subDomain,
+                   fieldType=fieldType,
+                   target=target,
+                   ttl=ttl)
+        else:
+            if zone_mappings[subDomain]['fieldType'] != fieldType:
+                self.delete_record(zone, subDomain)
+                return self.add_record(zone, subDomain, fieldType, target, ttl)
+
+            record_id = zone_mappings[subDomain]['id']
+            self._client.put('/domain/zone/{}/record/{}'.format(zone, record_id),
+                   target=target,
+                   ttl=ttl)
+
+        self.refresh_zone(zone)
+
+    def update_record(self, zone, subDomain, fieldType, target, ttl=0):
+        # Returns True if it updated the record, False otherwise
+        zone_mappings = self.get_zone_mappings(zone)
+
+        if subDomain not in zone_mappings:
+            raise Exception("SubDomain {} cannot be updated as it does not exists.".format(subDomain))
+
+        s = zone_mappings[subDomain]
+        if s['fieldType'] != fieldType:
+            self.delete_record(zone, subDomain)
+            self.add_record(zone, subDomain, fieldType, target, ttl)
+            return True
+        elif s['target'] != target or s['ttl'] != ttl:
+            self._client.put('/domain/zone/{}/record/{}'.format(zone, s['id']),
+                    subDomain=subDomain,
+                    fieldType=fieldType,
+                    target=target,
+                    ttl=ttl)
+            return True
+        return False
+
+        self.refresh_zone(zone)
+
+
+    def delete_record(self, zone, subDomain):
+        zone_mappings = self.get_zone_mappings(zone)
+
+        if subDomain not in zone_mappings:
+            return
+
+        record_id = zone_mappings[subDomain]['id']
+        self._client.delete('/domain/zone/{}/record/{}'.format(zone, record_id))
+
+        del self._cache[zone][subDomain]
+
+        self.refresh_zone(zone)
+
 
 
 conf = json.load(open('conf.json'))
 
-client = ovh.Client(
-    endpoint=conf['ovh_ids']['endpoint'],
-    application_key=conf['ovh_ids']['app_key'],
-    application_secret=conf['ovh_ids']['app_secret'],
-    consumer_key=conf['ovh_ids']['consumer_key']
-)
+ovh_client = OvhClient(
+                    endpoint=conf['ovh_ids']['endpoint'],
+                    application_key=conf['ovh_ids']['app_key'],
+                    application_secret=conf['ovh_ids']['app_secret'],
+                    consumer_key=conf['ovh_ids']['consumer_key'])
+#ovh_client.add_record('fgh.ovh', 'test.j1', 'TXT', '"zzzzz"')
+ovh_client.create_and_update_domains(conf['domains'])
 
-for domain in conf['domains']:
-    print("Updating {}...".format(domain['url']))
-    prfx =  '/domain/zone/{}'.format(domain['url'])
-    
-    entries_mappings = {client.get(prfx + '/record/{}'.format(record_id))['subDomain']: record_id for record_id in client.get(prfx + '/record')}
-    print(entries_mappings)
+for certificate in conf['certificates']:
+    acme = ACMEclient(certificate['cn'],
+            dns_class=OVHDns(ovh_client),
+            domain_alt_names=certificate['alt_names'],
+            registration_recovery_email='contact@juliendubiel.net',
+            account_key=None,
+            bits=4096,
+            digest='sha256',
+            ACME_CHALLENGE_WAIT_PERIOD=60)
+
+    certificate = acme.cert()
+    certificate_key = acme.certificate_key
+    account_key = acme.account_key
+
+    with open('certificates/{}.crt'.format(certificate['name']), 'w') as certificate_file:
+        certificate_file.write(certificate)
+
+    with open('certificates/{}.key'.format(certificate['name']), 'w') as certificate_key_file:
+        certificate_key_file.write(certificate_key)
+
+
+    # 2. to renew a certificate:
+    # with open('account_key.key', 'r') as account_key_file:
+    #     account_key = account_key_file.read()
+
+    # acme = sewer.Client(domain_name='example.com',
+    #                       dns_class=dns_class,
+    #                       account_key=account_key)
+    # certificate = acme.renew()
+    # certificate_key = acme.certificate_key
